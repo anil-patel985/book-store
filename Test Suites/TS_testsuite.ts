8a0b56973e6ba3a21effb6e08f94f3fa68799b67
lenovo <hardikbaraiya10hmb@gmail.com>
2021-03-25 20:25:23 +0530

    commit

---
+++ Test Suites/TS_testsuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2ee626e8-f389-492a-9d37-08631ded5aa8</testSuiteGuid>
+</TestSuiteEntity>
